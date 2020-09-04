679f24c88d3424994b7d94ec8696c34605020d86
kousuke1201abe <k-abe@giftee.co>
2020-09-05 00:02:33 +0900

    apply position fixed to sns links

--- src/components/SnsLinks.tsx
+++ src/components/SnsLinks.tsx
@@ -10,16 +10,16 @@ import {
 
 const SnsLinks = ({url}) => {
   return (
-    <div>
-    <FacebookShareButton url={url} style={{margin: "5px"}}>
-      <FacebookIcon size={40} round />
-    </FacebookShareButton>
-    <TwitterShareButton url={url} style={{margin: "5px"}}>
-      <TwitterIcon size={40} round />
-    </TwitterShareButton>
-    <LineShareButton url={url} style={{margin: "5px"}}>
-      <LineIcon size={40} round />
-    </LineShareButton>
+    <div className="sns-links">
+      <FacebookShareButton url={url} style={{margin: "5px"}}>
+        <FacebookIcon size={40} round />
+      </FacebookShareButton>
+      <TwitterShareButton url={url} style={{margin: "5px"}}>
+        <TwitterIcon size={40} round />
+      </TwitterShareButton>
+      <LineShareButton url={url} style={{margin: "5px"}}>
+        <LineIcon size={40} round />
+      </LineShareButton>
     </div>
   )
 }
