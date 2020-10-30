a62652b2f4a2b2e7c8d198301b9b42ad40e106ef
kousuke1201abe <k-abe@giftee.co>
2020-10-30 14:12:26 +0900

    setup contact form

--- src/pages/contact/thanks.tsx
+++ src/pages/contact/thanks.tsx
@@ -7,13 +7,13 @@ export default () => (
       <div className="container">
         <section className="section">
           <div className="columns">
-            <div className="column is-10 is-offset-1 has-text-centered" style={{ justifyContent: "center", margin: "20px" }}>
-              <h1 style={{ marginTop: '0px' }}>Thank you!</h1>
+            <div className="column has-text-centered" style={{ justifyContent: "center", margin: "20px" }}>
+              <h1 style={{ marginTop: '0px' }}>送信が完了しました。</h1>
               <p>ご連絡ありがとうございます。ご返信が必要な場合、担当の者が追ってご連絡致しますので、少々お待ちください。</p>
             </div>
           </div>
-          </section>
-        </div>
+        </section>
       </div>
+    </div>
   </Layout>
 )
