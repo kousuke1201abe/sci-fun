065fe4b1d8e1bd33e3939d2910da394ccbe4aae6
bonobozzzz <kawaiiyumegirl@gmail.com>
2020-11-29 21:52:40 +0900

    remove unnecessary code from Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,19 +2,8 @@ import React from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 import './all.sass';
-import useSiteMetadata from './SiteMetadata';
-import { withPrefix } from 'gatsby';
-
-interface SiteMetadataType {
-  title: string;
-  description: string;
-}
 
 const TemplateWrapper = ({ children }) => {
-  const {
-    title,
-    description,
-  }: SiteMetadataType = useSiteMetadata();
   return (
     <div style={{ maxWidth: '100%' }}>
       <Navbar />
